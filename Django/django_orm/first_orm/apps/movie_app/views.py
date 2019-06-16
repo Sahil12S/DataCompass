95c8164c92948fb019c2294ac06aedf5618e280f
Sahil Sharma <sahil.sharma@sjsu.edu>
2019-06-16 13:20:37 -0700

    one to many relationship

--- Django/django_orm/first_orm/apps/movie_app/views.py
+++ Django/django_orm/first_orm/apps/movie_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Movies
 
-# Create your views here.
+
+def index(request):
+    context = {
+        "all_movies": Movies.objects.all()
+    }
+    return render(request, "movie_app/index.html", context)
